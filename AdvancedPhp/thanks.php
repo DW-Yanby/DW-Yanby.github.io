750cffa0f9bdd0ccc2409c04570f0ea8cae67c27
DW-Yanby <76435886+DW-Yanby@users.noreply.github.com>
2021-06-07 17:20:46 +0200

    Update thanks.php
    Done remaining changes for filters.
    Changed all elements to function preg_match.
    Conditions added
    Bring back first version for echoing.

--- AdvancedPhp/thanks.php
+++ AdvancedPhp/thanks.php
@@ -1,17 +1,33 @@
 <?php
 //Méthodes de filtres
-  $pattern = '/[^a-zA-Z0-9'  . " " . ']/u';
+  $pattern = '/[a-zA-Z]$/u';
   $numberPattern = '/0+[0-9]{9}$/im';
-  $mailPattern = '/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/im';
-  $messagePattern = '/[^a-zA-Z0-9,.:!?;€'  . " " . ']/u';
+  $mailPattern = '/[-0-9a-zA-Z.+_]+@+[-0-9a-zA-Z.+_]+\.+[a-zA-Z]{2,4}$/u';
+  $messagePattern = '/[a-zA-Z0-9.,;\/:!?&\"\'\(\)-*+]$/u';
 //Eléments à filtrer
-  $firstName = preg_replace($pattern, '', $_POST['user_firstName']);
-  $name = preg_replace($pattern, '', $_POST['user_name']);
-  $phone = preg_replace($numberPattern, '', $_POST['user_phone']);
-  $message = preg_replace($messagePattern, '', $_POST['user_message']);
-  $mail = preg_replace($mailPattern, '', $_POST['user_email']);
-
-  echo "<p>Merci ", $firstName, " ", $name, ' de nous avoir contacté à propos de "', $_POST['user_subject'], '".</p>';
-  echo "<p>Un de nos conseiller vous contactera soit à l'adresse ", $mail, " ou par téléphone au ", $phone, " dans les plus brefs délais pour traiter votre demande :</p>";
-  echo "<p>", $message, "</p>";
+  $firstName = preg_match($pattern, $_POST['user_firstName']);
+  $name = preg_match($pattern, $_POST['user_name']);
+  $phone = preg_match($numberPattern, $_POST['user_phone']);
+  $message = preg_match($messagePattern, $_POST['user_message']);
+  $mail = preg_match($mailPattern, $_POST['user_email']);
+  
+  if (!($firstName)) {
+    echo "<p>Champ prénom invalide</p>"
+  };
+  if (!($name)) {
+    echo "<p>Champ nom invalide</p>"
+  };
+  if (!($phone)) {
+    echo "<p>Champ téléphone invalide</p>"
+  };
+  if (!($message)) {
+    echo "<p>Champ message invalide</p>"
+  };
+  if (!($mail)) {
+    echo "<p>Champ courriel invalide</p>"
+  };
+  if (($firstName && $name && $phone && $message && $mail)) {
+  echo "<p>Merci ", $_POST['user_firstName'], " ", $_POST['user_name'], ' de nous avoir contacté à propos de "', $_POST['user_subject'], '".</p>';
+  echo "<p>Un de nos conseiller vous contactera soit à l'adresse ", $_POST['user_email'], " ou par téléphone au ", $_POST['user_phone'], " dans les plus brefs délais pour traiter votre demande :</p>";
+  echo "<p>", $_POST['user_message'], "</p>";};
 ?>
