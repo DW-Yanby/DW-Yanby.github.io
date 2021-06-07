06809c31739fcda11994f3aa985ef16746194bd9
DW-Yanby <76435886+DW-Yanby@users.noreply.github.com>
2021-06-07 14:30:02 +0200

    Update thanks.php
    Added filters for quest 2 from advanced php questline

--- AdvancedPhp/thanks.php
+++ AdvancedPhp/thanks.php
@@ -1,5 +1,17 @@
 <?php
-  echo "<p>Merci ", $_POST['user_firstName'], " ", $_POST['user_name'], ' de nous avoir contacté à propos de "', $_POST['user_subject'], '".</p>';
-  echo "<p>Un de nos conseiller vous contactera soit à l'adresse ", $_POST['user_email'], " ou par téléphone au ", $_POST['user_telephone'], " dans les plus brefs délais pour traiter votre demande :</p>";
-  echo "<p>", $_POST['user_message'], "</p>";
+//Méthodes de filtres
+  $pattern = '/[^a-zA-Z0-9'  . " " . ']/u';
+  $numberPattern = '/[^0-9'  . " " . ']/u';
+  $mailPattern = '/[^a-zA-Z0-9.@'  . " " . ']/u';
+  $messagePattern = '/[^a-zA-Z0-9,.:!?;€'  . " " . ']/u';
+//Eléments à filtrer
+  $firstName = preg_replace($pattern, '', $_POST['user_firstName']);
+  $name = preg_replace($pattern, '', $_POST['user_name']);
+  $phone = preg_replace($numberPattern, '', $_POST['user_phone']);
+  $message = preg_replace($messagePattern, '', $_POST['user_message']);
+  $mail = preg_replace($mailPattern, '', $_POST['user_email']);
+
+  echo "<p>Merci ", $firstName, " ", $name, ' de nous avoir contacté à propos de "', $_POST['user_subject'], '".</p>';
+  echo "<p>Un de nos conseiller vous contactera soit à l'adresse ", $mail, " ou par téléphone au ", $phone, " dans les plus brefs délais pour traiter votre demande :</p>";
+  echo "<p>", $message, "</p>";
 ?>
